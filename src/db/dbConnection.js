da969b07c050caca2a6f6bde2e7bdfbc2463ed2a
Alok Kumar <alokkumar01218@gmail.com>
2024-12-12 12:04:07 +0530

    Util codes for custom responses and error

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -5,7 +5,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.CONNETION_STRING}/${DB_NAME}`)
-        console.log(`\n MongoDB connected: ${connectionInstance.connection.host}`);
+        console.log(`\nMongoDB connected: ${connectionInstance.connection.host}`);
         
     } catch (error) {
         console.log("Error: ", error);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import connectDB from "./db/dbConnection.js";
 import dotenv from "dotenv";
+import app from "./app.js";
 dotenv.config({
     path: "./env"
 })
@@ -12,4 +13,14 @@ dotenv.config({
 //     }
 // })()
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+        
+    })
+})
+.catch((error) => {
+    console.log("mongoDB connection error: ", error);
+    
+})

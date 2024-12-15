cf4db8aaebc92b9a5390cf671bcaeb48ee61c188
Alok Kumar <alokkumar01218@gmail.com>
2024-12-15 17:55:56 +0530

    Routes and controller setup

--- src/app.js
+++ src/app.js
@@ -18,8 +18,20 @@ app.use(
 );
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//routes import 
+// you can give any random name only when it is export default
+import userRouter from "./routes/user.routes.js"
+
+
+// routes usage
+app.use('/api/v1/users', userRouter)
+
+
+
+
+
+
 export default app;
